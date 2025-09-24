57afa3906a9dab81ee909f41bf1c93e93566e388
Kris Bowman <129105342+kris26658@users.noreply.github.com>
2025-09-24 06:39:54 -0700

    Update from main

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const http = require('http');
-const io = require('socket.io');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
+const { Server } = require('http');
 const app = express();
 const port = 3000;
+const http = require('http')
+const io = require('socket.io')(http);
+
+const server = http.createServer(
+    (req, res) => {
+        console.log('Request received');
+    }
+);
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -33,11 +39,23 @@ const db = new sqlite3.Database(dbPath, (err) => {
     } else {
         console.log('Connected to the SQLite database.');
         db.run(`CREATE TABLE IF NOT EXISTS access (
-            "User"	INTEGER NOT NULL UNIQUE,
+            "User"	INTEGER NOT NULL,
 	        "Classes"	TEXT NOT NULL UNIQUE,
 	        "Lists"	TEXT NOT NULL,
-	        "Questions"	TEXT NOT NULL,
-	        PRIMARY KEY("User" AUTOINCREMENT)
+	        PRIMARY KEY("Lists" AUTOINCREMENT)
+        )`, (err) => {
+            if (err) {
+                console.error('Error creating table:', err.message);
+            }
+        });
+
+        db.run(`CREATE TABLE IF NOT EXISTS Questions(
+            "List" TEXT NOT NULL,
+            "Question" TEXT NOT NULL,
+            "Answer1" TEXT NOT NULL,
+            "Answer2" TEXT NOT NULL,
+            "Answer3" TEXT,
+            "Answer4" TEXT
         )`, (err) => {
             if (err) {
                 console.error('Error creating table:', err.message);
@@ -61,6 +79,17 @@ socket.on('connection', (socket) => {
             }
         });
     });
+
+    socket.on('addQuestion', () => {
+        db.all(`SELECT * FROM Questions`, [], (err, rows) => {
+            if(err) {
+                console.error('Error fetching questions:', err.message);
+                socket.emit('questionsData', { error: 'Error fetching questions' });
+            } else {
+                socket.emit('questionsData', rows);
+            }
+        });
+    });
 });
 */
 
@@ -69,19 +98,30 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/add', (req, res) => {
-    res.render('addQ');
+app.get('/addQuestion', (req, res) => {
+    res.render('addQuestion');
 });
 
-app.post('/add', (req, res) => {
-    const Qdata = {
-        question : req.body.addQ,
-        answer1 : req.body.answer1,
-        answer2 : req.body.answer2,
-        answer3 : req.body.answer3,
-        answer4 : req.body.answer4}
-    
-    console.log(Qdata);
+app.post('/addQuestion', (req, res) => {
+    questionData = {
+        List : req.body.List,
+        question: req.body.addQuestion,
+        answer1: req.body.answer1,
+        answer2: req.body.answer2,
+        answer3: req.body.answer3,
+        answer4: req.body.answer4
+    }
+
+
+    db.run(`INSERT INTO Questions (List, Question, Answer1, Answer2, Answer3, Answer4) VALUES (?,?,?,?,?,?)`, questionData, function (err) {
+        if (err) {
+            console.error('Error inserting quiz:', err.message);
+        } else {
+            console.log(`A new question has been inserted`);
+        }
+    }
+    )
+    res.redirect('addQuestion');
 });
 
 app.get('/class', (req, res) => {
@@ -93,10 +133,89 @@ app.get('/quizzes', (req, res) => {
     res.render("quizzes.ejs")
 });
 
-app.get('/view',(req, res) => {
+app.post('/quizzes', (req, res) => {
+    var quizName = req.body.quizName;
+    if (quizName) {
+        db.run(`INSERT INTO access (User, Classes, Lists) VALUES (?,?,?)`, [1, 'Sample Class', quizName], function (err) {
+            if (err) {
+                console.error('Error inserting quiz:', err.message);
+            } else {
+                console.log(`A new quiz has been inserted with id ${this.lastID}`);
+            }
+        });
+    }
+});
+
+app.get('/viewQuiz', (req, res) => {
     res.render('viewQuiz.ejs')
 })
 
+
+app.get('/viewQuestion', (req, res) => {
+    res.render('viewQuestion.ejs')
+});
+
+app.get('/queue', (req, res) => {
+    res.render('queue.ejs')
+})
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+// FORMBAR STUFF
+const FORMBAR_URL = 'http://172.16.3.159:420/';
+const API_KEY = '0c396ef91f031c6a36624a832487f55930f0c4dc8e414a0d49711aa47e845101';
+
+const socket = new Server(server)
+
+socket.on('connect', () => {
+    console.log('Connected');
+    socket.emit('getActiveClass');
+});
+
+socket.on('setClass', (newClassId) => {
+    console.log(`The user is currently in the class with id ${newClassId}`);
+});
+
+let classId = 1; // Class Id here
+let classCode = 'vmnt' // If you're not already in the classroom, you can join it by using the class code.
+socket.emit('joinClass', classId);
+socket.on('joinClass', (response) => {
+    // If joining the class is successful, it will return true.
+    if (response == true) {
+        console.log('Successfully joined class')
+        socket.emit('classUpdate')
+    } else {
+        // If not, try to join the classroom with the class code.
+        socket.emit('joinRoom', classCode);
+        console.log('Failed to join class: ' + response)
+    }
+});
+
+socket.on('classUpdate', (classroomData) => {
+    console.log(classroomData);
+});
+
+// True or false 
+socket.emit('pollResp', 'True')
+
+// Text response
+socket.emit('pollResp', '', 'Text response here')
+
+socket.on('connect_error', (error) => {
+    /*
+        "xhr poll error" is just the error it give when it can't connect,
+        which is usually when the Formbar is not on or you are not on the same network.
+    */
+    if (error.message == 'xhr poll error') {
+        console.log('no connection');
+    } else {
+        console.log(error.message);
+    }
+
+    setTimeout(() => {
+        socket.connect();
+    }, 5000);
 });
